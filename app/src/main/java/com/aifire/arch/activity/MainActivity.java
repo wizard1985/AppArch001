bd17242dd07cd9723f398d213f768db41daad34b
wizardly1985 <wizard19850522@163.com>
2017-12-22 13:59:21 +0800

    新建工程

---
+++ app/src/main/java/com/aifire/arch/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.aifire.arch.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aifire.arch.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
